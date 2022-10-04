ee07a4be6e95fdb259965749b9687a1d85695417
kostya <klyosov03@mail.ru>
2022-10-04 16:21:42 +0300

    add coment

--- src/Palidrome.java
+++ src/Palidrome.java
@@ -1,10 +1,13 @@
+// позволяет работать с палиндромами
 public class Palidrome {
+    // перебирает аргументы командной строки
     public static void main(String[] args){
         for(int i=0; i<args.length;i++){
             String s = args[i];
             System.out.println(s +"  " + isPalindrome(s));
         }
     }
+    // Переворачивает строку
     public static String reverseString(String str){
         String s = "";
         for (int i=str.length()-1; i>-1;i--){
@@ -12,6 +15,7 @@ public class Palidrome {
         }
         return s;
     }
+    // проверяет является ли строка палиндромом
     public static boolean isPalindrome(String str){
         if (str.equals(reverseString(str))){
             return true;
--- src/Primes.java
+++ src/Primes.java
@@ -1,10 +1,14 @@
+// позволяет работать с простыми числами
 public class Primes {
+    // функция проверяет является ли число простым в интервале от 2 до 100
     public static void main(String[] args){
         for ( int i=2; i<=100; i++){
             if (isPrime(i)) System.out.println(i);
         }
     }
+    // проверяет делители числа
     public static boolean isPrime(int x){
+        if (x<=1);
         int count=0;
         for(int i=1; i<=x; i++){
             if (x%i==0){
